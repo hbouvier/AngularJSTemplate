54ec09966d6530a1ba2cd2b6511e7276d264bb3d
Henri Bouvier <henri@omsignal.com>
2014-08-03 10:41:34 -0400

    All resources are local and added xeditable

--- template/js/services/f_application-services.js
+++ template/js/services/f_application-services.js
@@ -1,6 +1,9 @@
 (function () {
     angular
-        .module('#A_APPLICATION#Services', ['ngResource'])
+        .module('#A_APPLICATION#Services', ['ngResource','xeditable'])
+        .run(function(editableOptions) {
+            editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+        })
         .filter('objectFilter', function ($rootScope) {
             return function (input, query) {
                 if (!query) return input;
@@ -9,6 +12,7 @@
                 angular.forEach(input, function (object) {
                     var copy = {};
                     var regex = new RegExp(query, 'im');
+                    console.log('object:', object, ', query:', query);
                     for (var i in object) {
                         // angular adds '$$hashKey' to the object.
                         if (object.hasOwnProperty(i) && i !== '$$hashKey')
